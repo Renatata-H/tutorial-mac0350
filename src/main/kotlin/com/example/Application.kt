c77ea5767d81e476331665f9e8e0efd6d6876e7d
Fernando Drekner de Souza <fernandodrekner@usp.br>
2025-05-14 20:14:43 -0300

    import certo application

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -5,13 +5,13 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.websocket.*
-import java.util.*
+import kotlin.time.Duration.Companion.seconds
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         install(WebSockets) {
             pingPeriod = 15.seconds
-            timeout = Duration.ofSeconds(60)
+            timeout = 60.seconds
             maxFrameSize = Long.MAX_VALUE
             masking = false
         }
